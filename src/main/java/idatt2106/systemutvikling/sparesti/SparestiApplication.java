b54ff7f689eccf4a2aeb6376c98c9a3f027a8693
BiggestGuz <Diblopro@gmail.com>
2024-04-16 09:51:59 +0200

    added maven, java and spring

---
+++ src/main/java/idatt2106/systemutvikling/sparesti/SparestiApplication.java
@@ -0,0 +1,13 @@
+package idatt2106.systemutvikling.sparesti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparestiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparestiApplication.class, args);
+	}
+
+}
