4df5f1bc99687154a9e9f7965f20dd201d25cd39
BiggestGuz <Diblopro@gmail.com>
2024-04-17 11:40:51 +0200

    Update filterchain to be not deprecated and a new base 64 secret key

--- src/main/java/idatt2106/systemutvikling/sparesti/SparestiApplication.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/SparestiApplication.java
@@ -2,8 +2,10 @@ package idatt2106.systemutvikling.sparesti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SparestiApplication {
 
 	public static void main(String[] args) {
