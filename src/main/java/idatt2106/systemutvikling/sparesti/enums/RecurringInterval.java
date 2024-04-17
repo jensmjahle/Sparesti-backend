1e68be283c72539a49c61b3149e0577985bd31a6
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-04-17 11:32:48 +0200

    Add RecurringInterval enum for challenge dao

---
+++ src/main/java/idatt2106/systemutvikling/sparesti/enums/RecurringInterval.java
@@ -0,0 +1,17 @@
+package idatt2106.systemutvikling.sparesti.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum RecurringInterval {
+  DAILY(24 * 60 * 60),
+  WEEKLY(7 * 24 * 60 * 60),
+  MONTHLY(30 * 24 * 60 * 60);
+
+  private final int seconds;
+
+  RecurringInterval(int seconds) {
+    this.seconds = seconds;
+  }
+
+}
