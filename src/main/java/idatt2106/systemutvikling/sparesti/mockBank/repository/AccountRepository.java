e28b47eb7d56b19fa4eb05c2d880bb596c169eb7
Torbjørn Antonsen <torbjant@ntnu.no>
2024-04-17 15:35:49 +0200

    Added method for finding AccountDAO from accountNr

--- src/main/java/idatt2106/systemutvikling/sparesti/mockBank/repository/AccountRepository.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/mockBank/repository/AccountRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<AccountDAO, Long> {
   //Optional: Add additional methods
   //AccountDAO findAccountDAOByCustomerDAO_Username();
+  Optional<AccountDAO> findAccountDAOByAccountNr(Long accountNr);
   List<AccountDAO> findAccountDAOSByCustomerDAO(CustomerDAO customer);
 }
