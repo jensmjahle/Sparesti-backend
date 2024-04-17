a62cecd21c80964fbb336719993d6d925d0acb04
Torbjørn Antonsen <torbjant@ntnu.no>
2024-04-17 15:36:34 +0200

    Added method for finding AccountDAO from accountNr

--- src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/AccountService.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/AccountService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AccountService {
@@ -19,6 +20,10 @@ public class AccountService {
     this.customerService = customerService;
   }
 
+  public AccountDAO findAccountByAccountNr(Long accountNumber) {
+    Optional<AccountDAO> accountDAOOptional = accountRepository.findAccountDAOByAccountNr(accountNumber);
+    return accountDAOOptional.orElseThrow(() -> new RuntimeException(accountNumber+" was not found"));
+  }
   public List<AccountDAO> findAccountsByUsername(String username) {
     CustomerDAO customer = customerService.findCustomerByUsername(username);
     return accountRepository.findAccountDAOSByCustomerDAO(customer);
