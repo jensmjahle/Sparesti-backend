463c3972973977d3e2114713935b516c6da5b7a7
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-04-17 13:08:48 +0200

    Minor changes

--- src/main/java/idatt2106/systemutvikling/sparesti/dao/ConditionDAO.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/dao/ConditionDAO.java
@@ -25,7 +25,7 @@ public class ConditionDAO {
   @SequenceGenerator(name = "conditionIdGenerator", sequenceName = "condition_sequence", allocationSize = 1)
   private Long conditionId;
 @ManyToOne (cascade = CascadeType.ALL)
-@JoinColumn(name = "achievementId", referencedColumnName = "conditionId")
+@JoinColumn(name = "conditionId", referencedColumnName = "achievementId")
 private AchievementDAO achievementDAO;
 private Long quantity;
   @Enumerated(EnumType.STRING)
