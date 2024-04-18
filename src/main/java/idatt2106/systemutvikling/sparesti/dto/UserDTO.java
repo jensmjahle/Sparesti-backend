7604db7617b800fd7305bf58e16b7c98ba1b00fc
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-04-18 11:23:39 +0200

    Add nullable attributes for UserDTO

--- src/main/java/idatt2106/systemutvikling/sparesti/dto/UserDTO.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/dto/UserDTO.java
@@ -3,6 +3,7 @@ package idatt2106.systemutvikling.sparesti.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 /**
  * Data transfer object for User
@@ -12,15 +13,25 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UserDTO {
   private String username;
+  @Nullable
   private String email;
+  @Nullable
   private String firstName;
+  @Nullable
   private String lastName;
+  @Nullable
   private String birthDate;
+  @Nullable
   private String profilePictureBase64;
+  @Nullable
   private Long monthlyIncome;
+  @Nullable
   private Long monthlySavings;
+  @Nullable
   private Long monthlyFixedExpenses;
+  @Nullable
   private Long currentAccount;
+  @Nullable
   private Long savingsAccount;
   private Boolean isConnectedToBank;
 
