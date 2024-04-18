dc124a308639e5f6c1fe8e381ada92615cd0ced7
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-04-18 08:56:22 +0200

    Create UserDTO

---
+++ src/main/java/idatt2106/systemutvikling/sparesti/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package idatt2106.systemutvikling.sparesti.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDTO {
+  private String username;
+  private String email;
+  private String firstName;
+  private String lastName;
+  private String birthDate;
+  private String profilePictureBase64;
+  private Long monthlyIncome;
+  private Long monthlySavings;
+  private Long monthlyFixedExpenses;
+  private Long currentAccount;
+  private Long savingsAccount;
+
+}
