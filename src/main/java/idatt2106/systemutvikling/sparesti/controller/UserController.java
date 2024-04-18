4e053a4d04e2fdd523dfcc469d4bd0cf95dd39ea
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-04-18 11:24:26 +0200

    Create endpoints for get, update og delete functions for users

---
+++ src/main/java/idatt2106/systemutvikling/sparesti/controller/UserController.java
@@ -0,0 +1,40 @@
+package idatt2106.systemutvikling.sparesti.controller;
+
+import idatt2106.systemutvikling.sparesti.dto.UserDTO;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import idatt2106.systemutvikling.sparesti.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  private final Logger logger = Logger.getLogger(UserController.class.getName());
+  private final UserService userService;
+
+  @Autowired
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @RequestMapping("/get/{username}")
+  public ResponseEntity<UserDTO> getUserDTO(@PathVariable String username) {
+    logger.info("Received request to get user information.");
+    return userService.getUserDTO(username);
+  }
+
+
+  @RequestMapping("/delete/{username}")
+  public ResponseEntity<UserDTO> deleteUserDTO(@PathVariable String username) {
+    logger.info("Received request to delete user information.");
+    return userService.deleteUserDTO(username);
+  }
+
+
+
+
+}
