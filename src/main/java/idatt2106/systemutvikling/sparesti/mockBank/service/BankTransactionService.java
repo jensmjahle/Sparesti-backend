af3eec200cc99735f51624fdb0cdb48557ee2a32
Håkon F. Fjellanger <haakonff@stud.ntnu.no>
2024-04-19 10:21:57 +0200

    Implement abstraction layer for transactions

--- src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/BankTransactionService.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/BankTransactionService.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class TransactionService implements TransactionServiceInterface {
+public class BankTransactionService implements TransactionServiceInterface {
 
   private final TransactionRepository transactionRepository;
   private final AccountService accountService;
