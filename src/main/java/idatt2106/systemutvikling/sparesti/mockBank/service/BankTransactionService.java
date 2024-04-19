423227d7e068b7b1f5d50ad25c266c09a7032c7d
Håkon F. Fjellanger <haakonff@stud.ntnu.no>
2024-04-19 10:16:15 +0200

    Rename TransactionService in the bank-abstraction layer, to BankTransactionService

--- src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/BankTransactionService.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/BankTransactionService.java
@@ -3,21 +3,27 @@ package idatt2106.systemutvikling.sparesti.mockBank.service;
 import idatt2106.systemutvikling.sparesti.mockBank.dao.AccountDAO;
 import idatt2106.systemutvikling.sparesti.mockBank.dao.TransactionDAO;
 import idatt2106.systemutvikling.sparesti.mockBank.repository.TransactionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import idatt2106.systemutvikling.sparesti.service.TransactionServiceInterface;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class TransactionService {
+@AllArgsConstructor
+public class TransactionService implements TransactionServiceInterface {
 
   private final TransactionRepository transactionRepository;
   private final AccountService accountService;
 
-  @Autowired
-  public TransactionService(TransactionRepository transactionRepository, AccountService accountService) {
-    this.transactionRepository = transactionRepository;
-    this.accountService = accountService;
+  @Override
+  public List<TransactionDAO> getLatestExpensesForAccountNumber(Long accountNumber, int page, int pageSize) {
+    Pageable pageable = PageRequest.of(page, pageSize, Sort.by("time").ascending());
+
+    return transactionRepository.findTransactionDAOByDebtorAccount(accountNumber, pageable);
   }
 
   public List<TransactionDAO> findTransactionsByAccountNr(Long accountNr) {
