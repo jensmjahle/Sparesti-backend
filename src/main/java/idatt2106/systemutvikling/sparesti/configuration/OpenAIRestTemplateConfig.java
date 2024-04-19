37afa90efcea67a7738ce08f94c6b602e170fdde
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-04-19 09:32:14 +0200

    Add http dependencies to pom

--- src/main/java/idatt2106/systemutvikling/sparesti/configuration/OpenAIRestTemplateConfig.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/configuration/OpenAIRestTemplateConfig.java
@@ -1,5 +1,6 @@
 package idatt2106.systemutvikling.sparesti.configuration;
 
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,16 +9,20 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class OpenAIRestTemplateConfig {
+  Logger logger = Logger.getLogger(OpenAIRestTemplateConfig.class.getName());
 
   @Value("${openai.api.key}")
   private String openaiApiKey;
+
 
   @Bean
   @Qualifier("openaiRestTemplate")
   public RestTemplate openaiRestTemplate() {
     RestTemplate restTemplate = new RestTemplate();
+    logger.info("OpenAI API Key: " + openaiApiKey);
+    logger.info(restTemplate.toString());
     restTemplate.getInterceptors().add((request, body, execution) -> {
-      request.getHeaders().add("Authorization", "Bearer " + openaiApiKey);
+     // request.getHeaders().add("Authorization", "Bearer " + openaiApiKey);
       return execution.execute(request, body);
     });
     return restTemplate;
