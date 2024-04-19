05979ab08507aa8daa6baab3c4c1702ff39a3215
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-04-19 10:32:00 +0200

    Create OpenAIException

---
+++ src/main/java/idatt2106/systemutvikling/sparesti/exceptions/OpenAIException.java
@@ -0,0 +1,12 @@
+package idatt2106.systemutvikling.sparesti.exceptions;
+
+public class OpenAIException extends RuntimeException {
+
+  public OpenAIException(String message) {
+    super(message);
+  }
+
+  public OpenAIException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
