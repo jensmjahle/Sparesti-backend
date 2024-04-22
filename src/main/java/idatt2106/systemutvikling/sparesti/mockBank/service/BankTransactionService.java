90b827b3a0932391d559937a3d102dc291bd3cd2
Håkon F. Fjellanger <haakonff@stud.ntnu.no>
2024-04-22 14:12:42 +0200

    First draft of extensive filtering on latestExpenses-functionality

--- src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/BankTransactionService.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/BankTransactionService.java
@@ -1,12 +1,23 @@
 package idatt2106.systemutvikling.sparesti.mockBank.service;
 
+import idatt2106.systemutvikling.sparesti.dao.UserDAO;
+import idatt2106.systemutvikling.sparesti.mapper.TransactionMapper;
+import idatt2106.systemutvikling.sparesti.mockBank.dao.AccountDAO;
+import idatt2106.systemutvikling.sparesti.mockBank.dao.CustomerDAO;
 import idatt2106.systemutvikling.sparesti.mockBank.dao.TransactionDAO;
+import idatt2106.systemutvikling.sparesti.mockBank.mapper.MockBankTransactionMapper;
 import idatt2106.systemutvikling.sparesti.mockBank.repository.TransactionRepository;
 import idatt2106.systemutvikling.sparesti.model.Transaction;
 import idatt2106.systemutvikling.sparesti.service.TransactionServiceInterface;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
+import jakarta.persistence.criteria.Order;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -19,15 +30,53 @@ import java.util.List;
 public class BankTransactionService implements TransactionServiceInterface {
 
   private final TransactionRepository transactionRepository;
+  private final AccountService accountService;
+
+  private final EntityManager entityManager;
 
   @Override
   public List<Transaction> getLatestExpensesForAccountNumber(@NonNull Long accountNumber, @Positive int page, @Positive int pageSize) {
-    Pageable pageable = PageRequest.of(page, pageSize, Sort.by("time").ascending());
+      List<AccountDAO> allAccountsForUser = accountService.findOtherAccountsOwnedBySameUser(accountNumber);
 
-    return transactionRepository
-            .findTransactionDAOByDebtorAccount(accountNumber, pageable)
+      if (allAccountsForUser == null)
+        return null;
+
+
+
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<TransactionDAO> criteriaQuery = criteriaBuilder.createQuery(TransactionDAO.class);
+    Root<TransactionDAO> root = criteriaQuery.from(TransactionDAO.class);
+
+    // A list of predicates each enforcing that the transactions creditor account is not equal the accounts owned by the user.
+    List<Predicate> creditorNotEqualOwnAccountList = allAccountsForUser
             .stream()
-            .map(BankTransactionService::toModel)
+            .map(AccountDAO::getAccountNr)
+            .map((Long a) -> { return criteriaBuilder.notEqual(root.get("creditorAccount"), a); })
+            .toList();
+
+    // The predicate filtering out all accounts where the creditor is not equal
+    Predicate creditorAccountNotOwn = creditorNotEqualOwnAccountList.getFirst();
+    for (Predicate p : creditorNotEqualOwnAccountList)
+      creditorAccountNotOwn = criteriaBuilder.and(creditorAccountNotOwn, p);
+
+    // Predicate only allowing transactions where the debtor is the parameter account.
+    Predicate debtorAccountEqualAccountNumber = criteriaBuilder.equal(root.get("debtorAccount"), accountNumber);
+
+    // The final predicate to use, only allowing transactions that are outgoing and not internal.
+    Predicate transactionIsExpense = criteriaBuilder.
+            and(debtorAccountEqualAccountNumber, creditorAccountNotOwn);
+
+
+
+    return entityManager
+            .createQuery(criteriaQuery.select(root)
+                    .where(transactionIsExpense)
+                    .orderBy(criteriaBuilder.desc(root.get("time"))))
+            .setFirstResult(page * pageSize)
+            .setMaxResults(pageSize)
+            .getResultList()
+            .stream()
+            .map(MockBankTransactionMapper::toModel)
             .toList();
   }
 
