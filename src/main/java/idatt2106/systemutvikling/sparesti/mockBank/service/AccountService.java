cbe6a58d76667cc059da324d26b8d34d18f29133
Håkon F. Fjellanger <haakonff@stud.ntnu.no>
2024-04-22 14:11:43 +0200

    Create function for finding other bank accounts with the same owner

--- src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/AccountService.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/AccountService.java
@@ -3,6 +3,7 @@ package idatt2106.systemutvikling.sparesti.mockBank.service;
 import idatt2106.systemutvikling.sparesti.mockBank.dao.AccountDAO;
 import idatt2106.systemutvikling.sparesti.mockBank.dao.CustomerDAO;
 import idatt2106.systemutvikling.sparesti.mockBank.repository.AccountRepository;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +28,17 @@ public class AccountService {
   public List<AccountDAO> findAccountsByUsername(String username) {
     CustomerDAO customer = customerService.findCustomerByUsername(username);
     return accountRepository.findAccountDAOSByCustomerDAO(customer);
+  }
+
+  public List<AccountDAO> findOtherAccountsOwnedBySameUser(@NonNull Long accountNumber) {
+    AccountDAO originalAccount = accountRepository.findAccountDAOByAccountNr(accountNumber).orElse(null);
+    if (originalAccount == null)
+      return null;
+
+    CustomerDAO c = customerService.findCustomerByUsername(originalAccount.getCustomerDAO().getUsername());
+    if (c == null)
+      return null;
+
+    return accountRepository.findAccountDAOSByCustomerDAO(c);
   }
 }
