71e2fdba977a678b8a108642c1e477e0b9b1d5e8
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-04-22 14:42:08 +0200

    Finalize GenerateChallengeService

--- src/main/java/idatt2106/systemutvikling/sparesti/service/challengeGeneration/GenerateChallengeService.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/service/challengeGeneration/GenerateChallengeService.java
@@ -1,14 +1,10 @@
 package idatt2106.systemutvikling.sparesti.service.challengeGeneration;
 
-import idatt2106.systemutvikling.sparesti.dao.ChallengeDAO;
 import idatt2106.systemutvikling.sparesti.dao.UserDAO;
-import idatt2106.systemutvikling.sparesti.enums.TransactionCategory;
-import idatt2106.systemutvikling.sparesti.model.ChallengeData;
-import idatt2106.systemutvikling.sparesti.model.Transaction;
+import idatt2106.systemutvikling.sparesti.model.challengeGeneration.ChallengeData;
 import idatt2106.systemutvikling.sparesti.repository.UserRepository;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +14,7 @@ public class GenerateChallengeService {
 
   private final UserRepository userRepository;
   private final Logger logger = Logger.getLogger(GenerateChallengeService.class.getName());
-  private ChallengeGenerator challengeGenerator;
+  private ChallengeGeneratorImpl challengeGenerator;
 
   @Autowired
   public GenerateChallengeService(UserRepository userRepository) {
@@ -34,7 +30,7 @@ public class GenerateChallengeService {
       List<UserDAO> users = userRepository.findAll();
       for (UserDAO user : users) {
         try {
-          challengeGenerator.generateDailyChallenges(initializeChallengeData(user));
+          challengeGenerator.generateChallenge(initializeChallengeData(user), 1);
         } catch (Exception e) {
           logger.severe("Failed to generate daily challenges for user " + user.getUsername() + "."
               + e.getMessage());
@@ -54,7 +50,7 @@ public class GenerateChallengeService {
       List<UserDAO> users = userRepository.findAll();
       for (UserDAO user : users) {
         try {
-          challengeGenerator.generateWeeklyChallenges(initializeChallengeData(user));
+          challengeGenerator.generateChallenge(initializeChallengeData(user), 7);
         } catch (Exception e) {
           logger.severe("Failed to generate weekly challenges for user " + user.getUsername() + "."
               + e.getMessage());
@@ -72,9 +68,10 @@ public class GenerateChallengeService {
   public void generateMonthlyChallenges() {
     try {
       List<UserDAO> users = userRepository.findAll();
+      int daysInMonth = LocalDateTime.now().toLocalDate().lengthOfMonth();
       for (UserDAO user : users) {
         try {
-          challengeGenerator.generateMonthlyChallenges(initializeChallengeData(user));
+          challengeGenerator.generateChallenge(initializeChallengeData(user), daysInMonth);
         } catch (Exception e) {
           logger.severe("Failed to generate monthly challenges for user " + user.getUsername() + "."
               + e.getMessage());
@@ -93,7 +90,7 @@ public class GenerateChallengeService {
       List<UserDAO> users = userRepository.findAll();
       for (UserDAO user : users) {
         try {
-          challengeGenerator.generateRandomChallenges(initializeChallengeData(user));
+          challengeGenerator.generateChallenge(initializeChallengeData(user), 1);
         } catch (Exception e) {
           logger.severe("Failed to generate random challenges for user " + user.getUsername() + "."
               + e.getMessage());
@@ -105,40 +102,21 @@ public class GenerateChallengeService {
   }
 
   /**
-   * Generates an instant challenge for a user.
+   * Generates a challenge for a user.
    *
    * @param user The user to generate the challenge for.
    */
   public void generateInstantChallenge(UserDAO user) {
     try {
-      challengeGenerator.generateInstantChallenge(initializeChallengeData(user));
+      challengeGenerator.generateChallenge(initializeChallengeData(user), 1);
     } catch (Exception e) {
       logger.severe("Failed to generate instant challenge for user " + user.getUsername() + "."
           + e.getMessage());
     }
   }
 
-  //todo: implement initializeChallengeData. Retrieve all necessary data from the database and return a ChallengeData object.
   public ChallengeData initializeChallengeData(UserDAO user) {
-
-    UserDAO userDAO;
-    List<ChallengeDAO> activeChallenges;
-    List<ChallengeDAO> inactiveChallenges;
-    int activeChallengesCount;
-    int inactiveChallengesCount;
-    double challengeCompletionRate;
-    double challengeAcceptanceRate;
-    double thisMonthTotalSavings;
-    double monthlySavingsGoal;
-    double monthlyIncome;
-    double monthlyFixedExpenses;
-    LocalDateTime startOfThisMonth;
-    LocalDateTime endOfThisMonth;
-    LocalDateTime currentDateTime;
-    List<Transaction> transactions;
-    Map<TransactionCategory, Double> categoryExpensesRatio;
-    Map<TransactionCategory, Double> pastChallengesByCategoryPercentage;
-    Map<TransactionCategory, Double> pastChallengesByCategoryAcceptedPercentage;
+    //todo: implement initializeChallengeData. Retrieve all necessary data from the database and return a ChallengeData object.
 
     return null;
   }
