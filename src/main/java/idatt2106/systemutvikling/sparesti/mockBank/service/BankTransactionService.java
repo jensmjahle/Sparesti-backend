390be8834f4c8379c2b7dd1173a6c70917eefd8a
Håkon F. Fjellanger <haakonff@stud.ntnu.no>
2024-04-22 10:19:58 +0200

    Setup parameter restrictions

--- src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/BankTransactionService.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/mockBank/service/BankTransactionService.java
@@ -4,7 +4,9 @@ import idatt2106.systemutvikling.sparesti.mockBank.dao.TransactionDAO;
 import idatt2106.systemutvikling.sparesti.mockBank.repository.TransactionRepository;
 import idatt2106.systemutvikling.sparesti.model.Transaction;
 import idatt2106.systemutvikling.sparesti.service.TransactionServiceInterface;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -19,7 +21,7 @@ public class BankTransactionService implements TransactionServiceInterface {
   private final TransactionRepository transactionRepository;
 
   @Override
-  public List<Transaction> getLatestExpensesForAccountNumber(Long accountNumber, int page, int pageSize) {
+  public List<Transaction> getLatestExpensesForAccountNumber(@NonNull Long accountNumber, @Positive int page, @Positive int pageSize) {
     Pageable pageable = PageRequest.of(page, pageSize, Sort.by("time").ascending());
 
     return transactionRepository
--- src/main/java/idatt2106/systemutvikling/sparesti/service/TransactionServiceInterface.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/service/TransactionServiceInterface.java
@@ -3,6 +3,8 @@ package idatt2106.systemutvikling.sparesti.service;
 import idatt2106.systemutvikling.sparesti.dto.TransactionDTO;
 import idatt2106.systemutvikling.sparesti.mockBank.dao.TransactionDAO;
 import idatt2106.systemutvikling.sparesti.model.Transaction;
+import jakarta.validation.constraints.Positive;
+import lombok.NonNull;
 
 import java.util.Date;
 import java.util.List;
@@ -16,5 +18,5 @@ public interface TransactionServiceInterface {
      * @param pageSize The size of each page in the pagination scheme.
      * @return A List of a page of outgoing transactions from the specified account.
      */
-    List<Transaction> getLatestExpensesForAccountNumber(Long accountNumber, int page, int pageSize);
+    List<Transaction> getLatestExpensesForAccountNumber(@NonNull Long accountNumber, @Positive int page, @Positive int pageSize);
 }
