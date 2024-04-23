03bb59dfc8500abdca6caaf9829a5831fbb58a2d
Håkon F. Fjellanger <haakonff@stud.ntnu.no>
2024-04-23 10:59:44 +0200

    Set column name on each field of MockBank entities

--- src/main/java/idatt2106/systemutvikling/sparesti/mockBank/dao/TransactionDAO.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/mockBank/dao/TransactionDAO.java
@@ -16,29 +16,39 @@ public class TransactionDAO {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "transactionIdGenerator")
+  @Column(name = "transactionTitle")
   private Long transactionId;
 
   @ManyToOne
   @JoinColumn(name = "accountNr")
   private AccountDAO accountDAO;
 
+  @Column(name = "transactionTitle")
   private String transactionTitle;
 
+  @Column(name = "time")
   @Temporal(TemporalType.TIMESTAMP)
   private Date time;
 
   @NotNull
+  @Column(name = "debtorAccount")
   private Long debtorAccount;
 
+  @Column(name = "debtorName")
   private String debtorName;
+
   @NotNull
+  @Column(name = "creditorAccount")
   private Long creditorAccount;
 
+  @Column(name = "creditorName")
   private String creditorName;
 
   @Positive
+  @Column(name = "amount")
   private Long amount;
 
+  @Column(name = "currency")
   private String currency;
 
 
