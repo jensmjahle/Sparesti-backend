bb2383f82798f5978043450464619138f0dee6c2
Håkon F. Fjellanger <haakonff@stud.ntnu.no>
2024-04-23 09:27:35 +0200

    Make AccountDAO conform to the planned schema

--- src/main/java/idatt2106/systemutvikling/sparesti/mockBank/dao/AccountDAO.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/mockBank/dao/AccountDAO.java
@@ -1,12 +1,6 @@
 package idatt2106.systemutvikling.sparesti.mockBank.dao;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,5 +23,15 @@ public class AccountDAO {
   @JoinColumn(name = "username", referencedColumnName = "username")
   private CustomerDAO customerDAO;
 
+  @Column(name = "balance")
+  private Long balance;
 
+  @Column(name = "accountName")
+  private String name;
+
+  @Column(name = "accountType")
+  private String type;
+
+  @Column(name = "currency")
+  private String currency;
 }
