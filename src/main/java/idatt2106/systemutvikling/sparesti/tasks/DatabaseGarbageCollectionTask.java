ea9e6c7c91a4910ca58a83e682d29a9520796a0b
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-04-25 15:41:12 +0200

    Create scheduled task for database garbage collection

--- src/main/java/idatt2106/systemutvikling/sparesti/tasks/DatabaseGarbageCollectionTask.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/tasks/DatabaseGarbageCollectionTask.java
@@ -1,10 +1,33 @@
 package idatt2106.systemutvikling.sparesti.tasks;
 
+import idatt2106.systemutvikling.sparesti.service.TransactionCategoryCacheService;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.logging.Logger;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@AllArgsConstructor
+@Component
 public class DatabaseGarbageCollectionTask {
 
   private final Logger logger = Logger.getLogger(DatabaseGarbageCollectionTask.class.getName());
+  private final TransactionCategoryCacheService transactionCategoryCacheService;
+
+  @Scheduled(cron = "${garbage.collection.cron.expression:0 0 0 * * *}")
+  public void runGarbageCollection() {
+    try {
+      logger.info("Running garbage collection on the database.");
+      LocalDate thirtyDaysAgo = LocalDate.now().minusDays(30);
+      Date thirtyDaysAgoDate = java.sql.Date.valueOf(thirtyDaysAgo);
+      transactionCategoryCacheService.deleteOutdatedCache(thirtyDaysAgoDate);
+    } catch (Exception e) {
+      logger.severe(
+          "An error occurred while running garbage collection on the database: " + e.getMessage());
+    }
+
+  }
 
 
 }
