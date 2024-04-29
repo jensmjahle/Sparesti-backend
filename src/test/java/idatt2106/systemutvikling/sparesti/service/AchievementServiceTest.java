78a4d9c5e3159e42efa0583d5284961a6d9b4d68
vildemv <vildemv@ntnu.no>
2024-04-29 12:48:52 +0200

    Sorted milestones and logs by date when retrieving from repository.

--- src/test/java/idatt2106/systemutvikling/sparesti/service/AchievementServiceTest.java
+++ src/test/java/idatt2106/systemutvikling/sparesti/service/AchievementServiceTest.java
@@ -18,7 +18,6 @@ import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
