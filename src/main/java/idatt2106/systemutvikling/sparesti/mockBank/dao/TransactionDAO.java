7285a9059a2ef8a713e6d3d7a9aecb49907b5ed0
Tini Tran <tinit@stud.ntnu.no>
2024-05-03 12:25:53 +0200

    checkstyled the dao classes for the mockbank

--- src/main/java/idatt2106/systemutvikling/sparesti/mockBank/dao/TransactionDAO.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/mockBank/dao/TransactionDAO.java
@@ -1,6 +1,15 @@
 package idatt2106.systemutvikling.sparesti.mockBank.dao;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.util.Date;
