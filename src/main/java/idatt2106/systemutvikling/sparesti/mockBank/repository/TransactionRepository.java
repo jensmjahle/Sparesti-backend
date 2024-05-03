3c133c3fef40b1a67eb93ec40448e42c717dd454
Tini Tran <tinit@stud.ntnu.no>
2024-05-03 12:24:57 +0200

    checkstyled the repository interfaces for the mockbank

--- src/main/java/idatt2106/systemutvikling/sparesti/mockBank/repository/TransactionRepository.java
+++ src/main/java/idatt2106/systemutvikling/sparesti/mockBank/repository/TransactionRepository.java
@@ -1,15 +1,12 @@
 package idatt2106.systemutvikling.sparesti.mockBank.repository;
 
 import idatt2106.systemutvikling.sparesti.mockBank.dao.AccountDAO;
-import idatt2106.systemutvikling.sparesti.mockBank.dao.CustomerDAO;
 import idatt2106.systemutvikling.sparesti.mockBank.dao.TransactionDAO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.domain.Pageable;
-
-
 import java.util.Date;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Repository interface for the Transaction entity.
@@ -29,13 +26,15 @@ public interface TransactionRepository extends JpaRepository<TransactionDAO, Lon
    * Method to find all transactions that belong to an account with a given account number.
    *
    * @param debtorAccountNumber the account number of the account
-   * @param pageable the page number
+   * @param pageable            the page number
    * @return a list of transaction entities as TransactionDAO
    */
-  List<TransactionDAO> findTransactionDAOByDebtorAccount(Long debtorAccountNumber, Pageable pageable);
+  List<TransactionDAO> findTransactionDAOByDebtorAccount(Long debtorAccountNumber,
+      Pageable pageable);
 
   /**
-   * Method to find all transactions that belong to an account with a given account number and a date limit.
+   * Method to find all transactions that belong to an account with a given account number and a
+   * date limit.
    *
    * @param accountNr the account number of the account
    * @param dateLimit the date limit
@@ -43,7 +42,7 @@ public interface TransactionRepository extends JpaRepository<TransactionDAO, Lon
    */
   List<TransactionDAO> findByAccountDAO_AccountNrAndTimeAfter(Long accountNr, Date dateLimit);
 
-    /**
+  /**
    * Method to find all transactions that belong to an account with a given account number.
    *
    * @param accountNr the account number of the account
